cc498d87bea625117c5165810fd8800be8033747
lumigamher <lmiguelgamboa@uts.edu.co>
2024-11-01 12:05:51 -0500

    feat: :sparkles: Correccion WbConfig y erres en el cors

--- src/main/java/com/projectcondor/condor/model/User.java
+++ src/main/java/com/projectcondor/condor/model/User.java
@@ -1,11 +1,14 @@
 package com.projectcondor.condor.model;
 
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Collection; 
+import java.util.Collection;
+import java.util.Date;
 
+@Data
 @Entity
 public class User implements UserDetails {
 
@@ -15,22 +18,30 @@ public class User implements UserDetails {
 
     private String username;
     private String password;
+    private String email;
+    
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    
+    private String address;
+    
+    @Column(name = "birth_date")
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+    
+    private String gender;
 
     // Este método debe devolver los roles o permisos (authorities) del usuario
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         // Aquí podrías implementar la lógica para devolver los roles o permisos
         return null;  // Devuelve los authorities como una colección de GrantedAuthority
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
     }
 
     @Override
@@ -51,10 +62,5 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
-    }
-
-    // Método para establecer la contraseña
-    public void setPassword(String password) {
-        this.password = password;  // Asigna la contraseña al campo privado
     }
 }
